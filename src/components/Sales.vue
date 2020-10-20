f858be357d89a5606ce9dd2f51a72c52a4cec150
Diego Hernández <dieherram@gmail.com>
2020-10-19 23:40:46 -0300

    commit

---
+++ src/components/Sales.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>Hay {{availableGames.length}} juegos con stock</p>
+    <list :hasButton="true" :games="availableGames" @sell-product="saleProduct"></list>
+  </div>
+</template>
+
+<script>
+import {mapGetters, mapActions} from 'vuex';
+import List from "@/components/List";
+export default {
+    components: {
+      List
+  },
+    computed: {
+        ...mapGetters(["availableGames"]),
+    },
+    methods: {
+      ...mapActions(["processSale"]),
+      saleProduct(payLoad){
+        this.processSale(payLoad.game);
+      },
+    },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Total.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Total de ventas</h1>
+    <h2>Tus juegos comprados:</h2>
+    <ul>
+      <li v-for="(number, game) in totalGamesSold" :key="game.id">{{game}} - {{number}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import{
+  mapGetters
+} from 'vuex'
+export default {
+  computed: {
+    ...mapGetters(['totalGamesSold'])
+  }
+}
+</script>
+
+<style>
+
+</style>
